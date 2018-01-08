d9811d593fdbaa7b7288c35570647f33c15bc24b
vpoturaev <viacheslav.poturaev@lazada.com>
2018-01-08 17:54:27 +0700

    more tests migrated

---
+++ tests/src/PHPUnit/CustomMappingTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Swaggest\SwaggerSchema\Tests\PHPUnit;
+
+use Swaggest\JsonSchema\Context;
+use Swaggest\JsonSchema\SwaggerSchema\Schema;
+use Swaggest\JsonSchema\SwaggerSchema\SwaggerSchema;
+use Swaggest\SwaggerSchema\Tests\Helper\CustomSchema;
+use Swaggest\SwaggerSchema\Tests\Helper\CustomSwaggerSchema;
+
+class CustomMappingTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMapping() {
+        $schema = CustomSwaggerSchema::import(json_decode(
+            file_get_contents(__DIR__ . '/../../../spec/petstore-swagger.json')
+        ));
+
+        $this->assertInstanceOf(CustomSchema::className(), $schema->definitions['User']);
+    }
+
+    public function testMappingWithContext() {
+        $context = new Context();
+        $context->objectItemClassMapping[Schema::className()] = CustomSchema::className();
+        $schema = SwaggerSchema::schema()->in(json_decode(
+            file_get_contents(__DIR__ . '/../../../spec/petstore-swagger.json')
+        ), $context);
+        $this->assertInstanceOf(CustomSchema::className(), $schema->definitions['User']);
+
+    }
+}
